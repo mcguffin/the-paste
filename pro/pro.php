388241b5efa7e4233eaa547c009681692660f17c
mcguffin <joern@flyingletters.com>
2017-11-20 19:03:30 +0100

    pro php

--- pro/pro.php
+++ pro/pro.php
@@ -1,3 +1,21 @@
 <?php
 
 // this is pro now
+namespace ThePaste;
+use ThePaste\Core;
+
+class ThePastePro extends Core\Singleton {
+	/**
+	 *	@inheritdoc
+	 */
+	protected function __construct() {
+		add_action('the_paste_enqueue_script',array($this,'enqueue_script'));
+	}
+
+	/**
+	 *	@action the_paste_enqueue_script
+	 */
+	public function enqueue_script() {
+		wp_enqueue_script();
+	}
+}
