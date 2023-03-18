df17fce922e32f9dade3bc1a6b939c733986b969
Don Bowman <don@agilicus.com>
2023-03-18 16:06:58 -0400

    fix: Update php8

--- include/ThePaste/Core/Singleton.php
+++ include/ThePaste/Core/Singleton.php
@@ -39,7 +39,8 @@ abstract class Singleton {
 	/**
 	 *	Prevent Instantinating
 	 */
-	private function __clone() { }
+	public function __clone() { }
+	public function __wakeup() { }
 
 	/**
 	 *	Protected constructor
